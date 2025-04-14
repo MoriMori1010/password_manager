93a1c29976573aa797bb235062ca26d4e6352db5
MoriMori1010 <yuhoshi.28@gmail.com>
2025-04-14 09:55:34 +0000

    パスワードを追加する関数の追加とパスワードを保存するファイルの作成

--- password-manager.sh
+++ password-manager.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+add_password(){
+read -p "サービス名を入力してください！" service_name
+read -p "ユーザー名を入力してください！" user_name
+read -p "パスワードを入力してください！" password
+echo
+
+cat << EOF >> file.txt
+サービス名: $service_name
+ユーザー名: $user_name
+パスワード: $password
+--------------------------------
+
+EOF
+
+echo"パスワードの追加は成功しました。"
+}
